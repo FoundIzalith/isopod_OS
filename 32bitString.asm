f6d3702efe3fc57e7d1ad6db1d30ed938a9ca3a2
FoundIzalith <b17kaylaeva@gmail.com>
2022-08-12 23:03:54 -0400

    Added 32 bit string

---
+++ 32bitString.asm
@@ -0,0 +1,25 @@
+[bits 32]
+VIDEO_MEMORY equ 0xb8000
+WHITE_ON_BLACK equ 0x0f
+
+print_string_pm:
+    pusha
+    mov edx, VIDEO_MEMORY ;sets edx to start of video memory 
+
+print_string_pm_loop:
+    mov al, [ebx]
+    mov ah, WHITE_ON_BLACK
+
+    cmp al, 0 ; if al == 0, then we're at the end of string
+    je done  ; so jump to done
+
+    mov [edx], ax
+
+    add ebx, 1
+    add edx, 2
+
+    jmp print_string_pm_loop
+
+print_string_pm_done:
+    popa
+    ret
