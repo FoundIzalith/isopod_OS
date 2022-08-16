f8cc544e725f9ccca7f8e4cb8883fd0f7700466e
FoundIzalith <b17kaylaeva@gmail.com>
2022-08-16 18:34:29 -0400

    Added BEGIN_PM and boot padding

--- 32bit-boot.asm
+++ 32bit-boot.asm
@@ -16,3 +16,16 @@
 %include "switch_to_pm.asm"
 
 [bits 32]
+
+BEGIN_PM:
+
+    mov ebx. MSG_PROT_MODE
+    call print_string_pm
+
+    jmp $
+
+MSG_REAL_MODE db "Started in 16-bit Real Mode", 0
+MSG_PROT_MODE db "Landed in 32-bit Protected Mode", 0
+
+times 510-($-$$) db 0
+dw 0xaa55
