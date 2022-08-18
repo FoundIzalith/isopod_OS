aaae5bde57abaa9125181779683506154618f74a
FoundIzalith <b17kaylaeva@gmail.com>
2022-08-18 12:25:00 -0400

    Added low level manager

---
+++ kernel/low_level.c
@@ -0,0 +1,22 @@
+//Reads byte from specified port
+unsigned char port_byte_in(unsigned short port) {
+    //Put AL register in variable 'result' when finished
+    //Load EDX with port
+    unsigned char result; 
+    __asm__("in %%dx, %%al" : "=a" (result) : "d" (port));
+    return result; 
+}
+
+void port_byte_out(unsigned short port, unsigned char data) {
+    __asm__("out %%al, %%dx" : :"a" (data), "d" (port));
+}
+
+unsigned short port_word_in(unsigned short port) {
+    unsigned short result;
+    __asm__("in %%dx, %%ax" : "=a" (result) : "d" (port));
+    return result;
+}
+
+void port_word_out(unsigned short port, unsigned short data) {
+    __asm__("out %%ax, %%dx" : :"a" (data), "d" (port));
+}
