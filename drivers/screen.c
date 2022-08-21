57b9a1e688c934ae7ac10137ca2fed825b268708
FoundIzalith <b17kaylaeva@gmail.com>
2022-08-21 18:15:45 -0400

    Finished screen

--- drivers/screen.c
+++ drivers/screen.c
@@ -1,8 +1,14 @@
 #include "screen.h"
+#include "../kernel/low_level.c"
+#include "../kernel/util.c"
 
 int get_screen_offset(int, int);
 int get_cursor_offset();
-int set_cursor(int);
+int get_cursor();
+void set_cursor(int);
+int handle_scrolling(int cursor_offset);
+void clear_screen();
+void print(char*);
 
 //Print char at col, row, or cursor position
 void print_char(char character, int col, int row, char attribute_byte) {
@@ -17,7 +23,7 @@ void print_char(char character, int col, int row, char attribute_byte) {
         offset = get_cursor();
     }
 
-    if(char == '\n') {
+    if(character == '\n') {
         int rows = offset / (2 * MAX_COLS);
         offset = get_screen_offset(79, rows);
     } else {
@@ -31,17 +37,73 @@ void print_char(char character, int col, int row, char attribute_byte) {
 }
 
 int get_screen_offset(int row, int col) {
-    return (2 * (row * MAX_COLS + col));
+    /*port_byte_out(REG_SCREEN_CTRL, 14);
+    port_byte_out(REG_SCREEN_DATA, (unsigned char)(offset >> 8));
+    port_byte_out(REG_SCREEN_CTRL, 15);
+
+    cursor_offset -= 2 * MAX_COLS;
+    return cursor_offset;*/
+
+    return 2 * (row * MAX_COLS + col);
+}
+
+int get_cursor() {
+    port_byte_out(REG_SCREEN_CTRL, 14);
+    int offset = port_byte_in(REG_SCREEN_DATA) << 8;
+    port_byte_out(REG_SCREEN_CTRL, 15);
+    offset += port_byte_in(REG_SCREEN_DATA);
+    return offset * 2;
 }
 
 void set_cursor(int offset) {
+    offset /= 2;
     port_byte_out(REG_SCREEN_CTRL, 14);
     port_byte_out(REG_SCREEN_DATA, (unsigned char)(offset >> 8));
     port_byte_out(REG_SCREEN_CTRL, 15);
+    port_byte_out(REG_SCREEN_DATA, (unsigned char)(offset & 0xff));
 }
 
-int get_cursor() 
-{
+void print_at(char* message, int col, int row) {
+    if (col >= 0 && row >= 0) set_cursor(get_screen_offset(col, row));
+
+    int i = 0;
+    while(message[i] != 0) {
+        print_char(message[i++], col, row, WHITE_ON_BLACK);
+    }
+}
+
+void print(char* message) {
+    print_at(message, -1, -1);
+}
+
+void clear_screen() {
+    int row = 0;
+    int col = 0;
+
+    for(row = 0; row < MAX_ROWS; row++) {
+        for (col = 0; col < MAX_COLS; col++) {
+            print_char(' ', col, row, WHITE_ON_BLACK);
+        }
+    }
+
+    set_cursor(get_screen_offset(0, 0));
+}
+
+int handle_scrolling(int cursor_offset) {
+    if (cursor_offset < MAX_ROWS * MAX_COLS * 2) return cursor_offset;
+
+    for(int i = 1; i < MAX_ROWS; i++) {
+        memory_copy(get_screen_offset(0, i) + VIDEO_ADDRESS,
+                    get_screen_offset(0, i-1) + VIDEO_ADDRESS,
+                    MAX_COLS * 2);
+    }
+
+    char* last_line = get_screen_offset(0, MAX_ROWS - 1) + VIDEO_ADDRESS;
+    for(int i = 0; i < MAX_COLS * 2; i++) {
+        last_line[i] = 0;
+    }
+
     cursor_offset -= 2 * MAX_COLS;
+
     return cursor_offset;
 }
